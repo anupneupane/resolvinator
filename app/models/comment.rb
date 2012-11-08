ed30b0cfb960ebb8d80f0bc43032d78ff37bca6f
bradwheel <bradley.m.wheel@gmail.com>
2012-11-08 10:40:03 -0500

    write first test for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,4 @@
 class Comment < ActiveRecord::Base
   attr_accessible :content, :is_answer
-
   belongs_to :issue
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,3 +1,14 @@
 require 'spec_helper'
 
 
+describe Comment do
+  it "is able to comment on an issue" do
+    issue = Issue.create(:title => "Why won't Joe let up?")
+    issue.comments.create(:content => "This is a test comment")
+
+
+    issue.comments.first.title should == "Why won't Joe let up?"
+  end
+end
+
+
